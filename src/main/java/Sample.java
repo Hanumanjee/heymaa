627156da3b55a35081b2afec79978693f5056dfb
ShivamHanumaanjeee <shivammacate@gmail.com>
2018-05-13 10:03:41 -0700

    delete //sssss

--- src/main/java/Sample.java
+++ src/main/java/Sample.java
@@ -2,9 +2,7 @@ public class Sample {
     public static void main(String[] arg){
         System.out.println("Hey Maa");
         System.out.println("Hey Maa, I love you Maaaaaa");
-        //ssss/
-        //sssss/
-        //sssssss
+
 
     //Should be done better way
     }
