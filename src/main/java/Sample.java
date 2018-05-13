a15cca0b40f191733560099fb0847277e94b8a38
ShivamHanumaanjeee <shivammacate@gmail.com>
2018-05-13 10:00:47 -0700

    delete line

--- src/main/java/Sample.java
+++ src/main/java/Sample.java
@@ -2,6 +2,9 @@ public class Sample {
     public static void main(String[] arg){
         System.out.println("Hey Maa");
         System.out.println("Hey Maa, I love you Maaaaaa");
+        //ssss/
+        //sssss/
+        //sssssss
 
     //Should be done better way
     }
