c1d27bb26be96e532920806545bb47787d3ceda5
ShivamHanumaanjeee <shivammacate@gmail.com>
2018-05-13 09:53:27 -0700

    add two line

--- src/main/java/Sample.java
+++ src/main/java/Sample.java
@@ -1,6 +1,8 @@
 public class Sample {
     public static void main(String[] arg){
         System.out.println("Hey Maa");
+        System.out.println("Hey Maa, I love you Maaaaaa");
+        System.out.println("Hey Maa, I love you Maaaaaa");
     //Should be done better way
     }
 }
