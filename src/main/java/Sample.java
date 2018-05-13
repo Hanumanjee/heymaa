13fda2087554ebbd49f6bf7bf170a068e5a78d67
ShivamHanumaanjeee <shivammacate@gmail.com>
2018-05-13 10:04:47 -0700

    add a line

--- src/main/java/Sample.java
+++ src/main/java/Sample.java
@@ -2,8 +2,8 @@ public class Sample {
     public static void main(String[] arg){
         System.out.println("Hey Maa");
         System.out.println("Hey Maa, I love you Maaaaaa");
+        //This line I will commit and revert
 
 
-    //Should be done better way
     }
 }
